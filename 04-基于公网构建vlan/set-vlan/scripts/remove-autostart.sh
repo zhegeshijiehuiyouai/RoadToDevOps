cbca4963fe37f7c8ef466a020fbacdfa1ded55a3
zhegeshijiehuiyouai <946649765@qq.com>
2019-09-18 11:02:40 +0800

    修复删除vlan时，本脚本添加的开机启动项没有删完的bug

--- 04-基于公网构建vlan/set-vlan/scripts/remove-autostart.sh
+++ 04-基于公网构建vlan/set-vlan/scripts/remove-autostart.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # 这是自定义的开机启动项
+# 设置开机启动的方法：
+# chmod +x /etc/rc.d/rc.local
+# echo "sh /root/BashShell/start-services.sh" >> rc.local
 grep "/root/BashShell/inet-based-vlan/vlan-set 1" /root/BashShell/start-services.sh
 if [ $? -eq 0 ]; then
-  sed -i /"\/root\/BashShell\/inet-based-vlan\/vlan-set 1"/d start-services.sh
+  sed -i /"\/root\/BashShell\/inet-based-vlan\/vlan-set 1"/d /root/BashShell/start-services.sh
 fi
 
 grep "/root/BashShell/inet-based-vlan/port-set 1" /root/BashShell/start-services.sh
