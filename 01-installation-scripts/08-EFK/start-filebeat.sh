9facb625ce45a3c2eeb204fae1b8910754fd05b8
zhegeshijiehuiyouai <946649765@qq.com>
2020-09-21 14:17:29 +0800

    添加多目录日志采集配置

--- 01-installation-scripts/08-EFK/start-filebeat.sh
+++ 01-installation-scripts/08-EFK/start-filebeat.sh
@@ -18,38 +18,54 @@ check_server_in_host kibana
 echo  # 换行，美观
 
 ######################################################
-log_dir=/data/myapp/tomcat/logs/catalina.out
-inner_log_dir=/data/logs/catalina.log
+log_dir=/data/myapp/logs
+inner_log_dir=/data/logs
 host=`hostname`
 current_dir=`pwd`
 image=docker.elastic.co/beats/filebeat:7.9.1
 
 [ -d ${current_dir}/filebeat ] || mkdir -p ${current_dir}/filebeat
-if [ ! -f ${current_dir}/filebeat/filebeat.yml ];then
+if [ ! -f "${current_dir}/filebeat/filebeat.yml" ];then
 echo -e "\033[33m写入配置文件：${current_dir}/filebeat/filebeat.yml\033[0m"
 cat > ${current_dir}/filebeat/filebeat.yml << EOF
 filebeat.inputs:
   - type: log
     enabled: true
     paths:
-      - ${inner_log_dir}
+      - ${inner_log_dir}/log1.log
+    fields:
+      type: "log1"
     multiline:
       pattern: '^[[:space:]]' # 所有的空白行，合并到前面不是空白的那行
       negate: false
       match: after
       timeout: 15s
       max_lines: 500
+# 打开以下注释和output中的注释，即可配置多目录日志采集
+#  - type: log
+#    enabled: true
+#    paths:
+#      - /data/logs/log2.log
+#    fields:
+#      type: "log2"
 
 setup.kibana:
   host: "kibana:5601"
 
 setup.dashboards.enabled: false
 setup.ilm.enabled: false
-setup.template.name: "${host}-catalina.out"       #顶格，和output对齐
-setup.template.pattern: "${host}-catalina.out*"   #顶格，和output对齐
+setup.template.name: "${host}"       #顶格，和output对齐
+setup.template.pattern: "${host}-*"   #顶格，和output对齐
 output.elasticsearch:
   hosts: ["elasticsearch:9200"]
-  index: "${host}-catalina.out-%{+yyyy.MM.dd}" #指定index name
+  indices:
+    - index: "${host}-log1-%{+yyyy.MM.dd}" #指定index name
+      when.equals:
+        fields.type: "log1"
+# 打开以下注释和input中的注释，即可配置多目录日志采集
+#    - index: "${host}-log2-%{+yyyy.MM.dd}" #指定index name
+#      when.equals:
+#        fields.type: "log2"
 EOF
 fi
 
