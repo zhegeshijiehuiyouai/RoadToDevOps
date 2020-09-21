11d8baba6589b5a36797b0be8f2e9842a1fb98e1
zhegeshijiehuiyouai <946649765@qq.com>
2020-09-21 10:04:13 +0800

    优化了搜索N天以前的索引的逻辑

--- 02-elasticsearch-tools/02-clean-date-format-es-index-by-date.sh
+++ 02-elasticsearch-tools/02-clean-date-format-es-index-by-date.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #
-# 针对按天拆分的索引（如test-log-2019.09.28），根据日期删除es索引
+# 针对按天拆分的索引（如test-log-2020.09.21），根据日期删除es索引
 # Author：zhegeshijiehuoyouai
-# Date：2019.9.28
 #
 
 # 定义 Elasticsearch 访问路径
@@ -10,7 +9,14 @@ http_es=10.0.17.98:9200
 # 定义索引保留天数
 keep_days=3
 # 定义要删除的索引，索引之间用空格隔开
-wanna_del_indexs=$(curl -s -XGET "http://${http_es}/_cat/indices" |awk '{print $3}' | grep -Ev "^\." | grep $(date -d "${keep_days} day ago" +%Y.%m.%d) | uniq)
+curl -w "\n" -s -XGET "http://${http_es}/_cat/indices" |awk '{print $3}' | grep -Ev "^\." >> _all_indexs
+for i in $(seq 1 ${keep_days});do
+    dateformat=$(date -d "${i} day ago" +%Y.%m.%d)
+    sed -i /${dateformat}/d _all_indexs
+done
+wanna_del_indexs=$(cat _all_indexs | grep -Ev "^[[:space:]]*$")
+rm -f _all_indexs
+
 # 定义关键字颜色
 color=32
 
@@ -20,21 +26,21 @@ echo
 
 # 定义删除函数
 clean_index(){
-  curl -s -XDELETE  http://${http_es}/$1
+    curl -s -XDELETE  http://${http_es}/$1
 }
 
 # 删除操作
 count=0
 for index in ${wanna_del_indexs}; do
-  # 清理索引
-  echo -e "清理 \033[${color}m${index}\033[0m 中..."
-  clean_index ${index} &>/dev/null # 不显示删除的详细信息
-  count=$[${count}+1]
-  echo "第 ${count} 条多余索引清理完毕"
+    # 清理索引
+    echo -e "清理 \033[${color}m${index}\033[0m 中..."
+    clean_index ${index} &>/dev/null # 不显示删除的详细信息
+    count=$[${count}+1]
+    echo "第 ${count} 条多余索引清理完毕"
 done
 
 # 如果有清理索引，那么报告一下总情况
 if [ ${count} -gt 0 ]; then
-  echo ""
-  echo -e "共清理 \033[${color}m${count}\033[0m 条索引\n所有多余所有均清理完毕，均保留了近 \033[${color}m${keep_days}\033[0m 天的索引"
+    echo ""
+    echo -e "共清理 \033[${color}m${count}\033[0m 条索引\n所有多余所有均清理完毕，保留了近 \033[${color}m${keep_days}\033[0m 天的索引"
 fi
