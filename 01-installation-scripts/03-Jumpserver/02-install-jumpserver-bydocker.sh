8107983cfa79e0ee86c025be413e87f5dbd29039
zhegeshijiehuiyouai <946649765@qq.com>
2020-09-22 17:33:15 +0800

    修复再次部署jumpserver会重新生成key的bug

--- 01-installation-scripts/03-Jumpserver/02-install-jumpserver-bydocker.sh
+++ 01-installation-scripts/03-Jumpserver/02-install-jumpserver-bydocker.sh
@@ -1,21 +1,26 @@
+#!/bin/bash
 # 生成key和token
-if [ ! "$SECRET_KEY" ]; then
-  SECRET_KEY=`cat /dev/urandom | tr -dc A-Za-z0-9 | head -c 50`;
-  echo "SECRET_KEY=$SECRET_KEY" >> ~/.bashrc;
-  echo $SECRET_KEY;
+source ~/.bashrc
+grep SECRET_KEY ~/.bashrc &> /dev/null
+if [ $? -ne 0 ]; then
+  SECRET_KEY=`cat /dev/urandom | tr -dc A-Za-z0-9 | head -c 50`
+  echo "SECRET_KEY=$SECRET_KEY" >> ~/.bashrc
+  echo $SECRET_KEY
 else
-  echo $SECRET_KEY;
-fi  
-if [ ! "$BOOTSTRAP_TOKEN" ]; then
-  BOOTSTRAP_TOKEN=`cat /dev/urandom | tr -dc A-Za-z0-9 | head -c 16`;
-  echo "BOOTSTRAP_TOKEN=$BOOTSTRAP_TOKEN" >> ~/.bashrc;
-  echo $BOOTSTRAP_TOKEN;
+  echo "SECRET_KEY已存在：${SECRET_KEY}"
+fi
+grep $BOOTSTRAP_TOKEN ~/.bashrc &> /dev/null
+if [ $? -ne 0 ]; then
+  BOOTSTRAP_TOKEN=`cat /dev/urandom | tr -dc A-Za-z0-9 | head -c 16`
+  echo "BOOTSTRAP_TOKEN=$BOOTSTRAP_TOKEN" >> ~/.bashrc
+  echo $BOOTSTRAP_TOKEN
 else
-  echo $BOOTSTRAP_TOKEN;
+  echo "BOOTSTRAP_TOKEN已存在：${BOOTSTRAP_TOKEN}"
 fi
 # docker启动命令
 [ -d /data/jumpserver/data ] || mkdir -p /data/jumpserver/data
 [ -d /data/jumpserver/mysql ] || mkdir -p /data/jumpserver/mysql
+echo -e "\033[32m\ndocker启动容器，容器名为jms_all\033[0m"
 docker run --name jms_all -d \
   -v /data/jumpserver/data:/opt/jumpserver/data \
   -v /data/jumpserver/mysql:/var/lib/mysql \
