0de27ab7c76fd696b0ed88c6f46d5e85bb5dcfd4
zhegeshijiehuiyouai <946649765@qq.com>
2020-11-17 19:39:05 +0800

    新增通过parted创建lvm脚本

---
+++ 04-disk-tools/02-Create-LVM/02-create-lvm-by-parted.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# only use in centos7
+partition=/data                # 定义最终挂载的名称
+vgname=vgdata                      # 定义逻辑卷组的名称
+lvmname=lvmdata                     # 定义逻辑卷的名称
+code='vdb'   # 根据分区的实际情况修改
+ 
+disk=
+for i in $code  
+do
+# 这里自动化完成了所有分区fdisk苦逼的交互步骤
+parted -s /dev/$i mklabel gpt
+parted /dev/$i << EOF          
+p
+mkpart
+${code}1
+xfs
+0%
+100% 
+toggle 1 lvm
+p
+q
+EOF
+disk="$disk /dev/${i}1" # 将所有分区拼起来
+echo $disk
+done
+ 
+pvcreate $disk
+pvdisplay
+vgcreate $vgname $disk
+vgdisplay
+lvcreate -l 100%VG -n $lvmname $vgname
+lvdisplay
+echo "start mkfs....."
+sleep 2
+mkfs.xfs /dev/$vgname/$lvmname
+if [ $? == 0 ]
+then 
+	mkdir -p $partition
+	echo "/dev/$vgname/$lvmname  $partition  xfs     defaults        0 0" >> /etc/fstab
+	mount -a
+	df -h
+	echo "lvm create and mount successful!"
+
+else
+
+	echo "lvm create fail!"
+fi
+
