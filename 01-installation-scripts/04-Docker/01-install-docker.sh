a4569620055af18c1398f3e052b355b56fbb153a
zhegeshijiehuiyouai <946649765@qq.com>
2020-11-19 13:57:27 +0800

    添加docker-compose部署脚本

--- 01-installation-scripts/04-Docker/01-install-docker.sh
+++ 01-installation-scripts/04-Docker/01-install-docker.sh
@@ -41,6 +41,23 @@ EOF
 systemctl start docker
 systemctl enable docker
 
+######### 部署docker-compose
+docker_compose_version=$(curl -s --connect-timeout 5 https://github.com/docker/compose/tags | grep "/docker/compose/releases/tag/" | head -1 | awk -F'"' '{print $2}' | xargs basename)
+# 接口正常，[ ! ${docker_compose_version} ]为1；接口失败，[ ! ${docker_compose_version} ]为0
+if [ ! ${docker_compose_version} ];then
+    echo -e "\033[31m[*] docker-compose github官网[ \033[36mhttps://github.com/docker/compose/tags\033[31m ]访问超时，请检查网络！\033[0m"
+    echo -e "\033[32m[>] docker状态：\033[0m"
+    systemctl status docker
+    echo -e "\033[32m[>] docker版本：\033[0m"
+    docker -v
+    exit 10
+fi
+echo -e "\033[32m[+] 部署docker-compose\033[0m"
+# 使用国内源加速下载
+curl -sL --connect-timeout 5 "https://get.daocloud.io/docker/compose/releases/download/${docker_compose_version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
 echo -e "\033[32m[>] docker状态：\033[0m"
 systemctl status docker
 echo -e "\033[32m[>] docker版本：\033[0m"
