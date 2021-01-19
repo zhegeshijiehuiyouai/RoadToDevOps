d64069b8f6db2f4cec4f9699560f5dc8c15522a0
zhegeshijiehuiyouai <946649765@qq.com>
2021-01-19 15:39:25 +0800

    配置阿里云仓库镜像

--- 01-installation-scripts/15-maven/01-install-maven.sh
+++ 01-installation-scripts/15-maven/01-install-maven.sh
@@ -105,5 +105,83 @@ echo "export MAVEN_HOME=${back_dir}/maven" >  /etc/profile.d/zookeeper.sh
 echo "export PATH=$PATH:${back_dir}/maven/bin" >> /etc/profile.d/zookeeper.sh
 source /etc/profile
 
+echo_info 配置仓库镜像地址
+cat > /tmp/.temp_repo_file <<EOF
+    <mirror>
+        <id>aliyun-public</id>
+        <mirrorOf>*</mirrorOf>
+        <name>aliyun public</name>
+        <url>https://maven.aliyun.com/repository/public</url>
+    </mirror>
+    <mirror>
+        <id>aliyun-central</id>
+        <mirrorOf>*</mirrorOf>
+        <name>aliyun central</name>
+        <url>https://maven.aliyun.com/repository/central</url>
+    </mirror>
+    <mirror>
+        <id>aliyun-spring</id>
+        <mirrorOf>*</mirrorOf>
+        <name>aliyun spring</name>
+        <url>https://maven.aliyun.com/repository/spring</url>
+    </mirror>
+    <mirror>
+        <id>aliyun-spring-plugin</id>
+        <mirrorOf>*</mirrorOf>
+        <name>aliyun spring-plugin</name>
+        <url>https://maven.aliyun.com/repository/spring-plugin</url>
+    </mirror>
+    <mirror>
+        <id>aliyun-apache-snapshots</id>
+        <mirrorOf>*</mirrorOf>
+        <name>aliyun apache-snapshots</name>
+        <url>https://maven.aliyun.com/repository/apache-snapshots</url>
+    </mirror>
+    <mirror>
+        <id>aliyun-google</id>
+        <mirrorOf>*</mirrorOf>
+        <name>aliyun google</name>
+        <url>https://maven.aliyun.com/repository/google</url>
+    </mirror>
+    <mirror>
+        <id>aliyun-gradle-plugin</id>
+        <mirrorOf>*</mirrorOf>
+        <name>aliyun gradle-plugin</name>
+        <url>https://maven.aliyun.com/repository/gradle-plugin</url>
+    </mirror>
+    <mirror>
+        <id>aliyun-jcenter</id>
+        <mirrorOf>*</mirrorOf>
+        <name>aliyun jcenter</name>
+        <url>https://maven.aliyun.com/repository/jcenter</url>
+    </mirror>
+    <mirror>
+        <id>aliyun-releases</id>
+        <mirrorOf>*</mirrorOf>
+        <name>aliyun releases</name>
+        <url>https://maven.aliyun.com/repository/releases</url>
+    </mirror>
+    <mirror>
+        <id>aliyun-snapshots</id>
+        <mirrorOf>*</mirrorOf>
+        <name>aliyun snapshots</name>
+        <url>https://maven.aliyun.com/repository/snapshots</url>
+    </mirror>
+    <mirror>
+        <id>aliyun-grails-core</id>
+        <mirrorOf>*</mirrorOf>
+        <name>aliyun grails-core</name>
+        <url>https://maven.aliyun.com/repository/grails-core</url>
+    </mirror>
+    <mirror>
+        <id>aliyun-mapr-public</id>
+        <mirrorOf>*</mirrorOf>
+        <name>aliyun mapr-public</name>
+        <url>https://maven.aliyun.com/repository/mapr-public</url>
+    </mirror>
+EOF
+sed -i '/\s<mirrors>/ r /tmp/.temp_repo_file' ${back_dir}/maven/conf/settings.xml
+rm -f /tmp/.temp_repo_file
+
 echo_info maven已部署完毕，版本信息如下：
 mvn -version
