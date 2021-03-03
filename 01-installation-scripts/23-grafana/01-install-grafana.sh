15be61e32789baec6c3351989ca0e9c27c52110a
zhegeshijiehuiyouai <946649765@qq.com>
2021-03-03 09:33:34 +0800

    调整输出信息

--- 01-installation-scripts/23-grafana/01-install-grafana.sh
+++ 01-installation-scripts/23-grafana/01-install-grafana.sh
@@ -193,7 +193,8 @@ EOF
     echo -e "\033[37m                  端口：${grafana_port}\033[0m"
     echo -e "\033[37m                  部署目录：${grafana_home}\033[0m"
     echo -e "\033[37m                  grafana访问地址：http://${machine_ip}:${grafana_port}/\033[0m"
-    echo -e "\033[37m                  推荐模板id - 主机基础监控：5276/\033[0m"
+    echo -e "\033[37m                  默认账号密码：admin / admin\033[0m"
+    echo -e "\033[37m                  推荐模板id - 主机基础监控：9276/\033[0m"
 }
 
 function config_grafana() {
