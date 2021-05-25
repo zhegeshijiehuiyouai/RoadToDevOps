76336b83d1af51879997559e84c146bb4985598e
unknown <946649765@qq.com>
2021-05-26 05:04:43 +0800

    删除过时文件

--- 01-installation-scripts/03-Jumpserver/01-install-jumpserver.sh
+++ 01-installation-scripts/03-Jumpserver/01-install-jumpserver.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-web_port=80
-ssh_port=2222
-
 # 带格式的echo函数
 function echo_info() {
     echo -e "[\033[36m$(date +%T)\033[0m] [\033[32mINFO\033[0m] \033[37m$@\033[0m"
@@ -14,47 +11,150 @@ function echo_error() {
     echo -e "[\033[36m$(date +%T)\033[0m] [\033[41mERROR\033[0m] \033[1;31m$@\033[0m"
 }
 
-# jumpserver_data_dir=/data/jumpserver/data
-# jumpserver_mysql_dir=/data/jumpserver/mysql
-# [ -d $jumpserver_data_dir ] || mkdir -p $jumpserver_data_dir
-# [ -d $jumpserver_mysql_dir ] || mkdir -p $jumpserver_mysql_dir
+SRC_DIR=$(pwd)/00src00
+MY_DIR=$(pwd)
+JUMP_HOME=jumpserver
+JUMP_GIT_DIR=Dockerfile
+JUMP_PORT=11880
 
-source ~/.bashrc
-echo_info 准备生成key和token
-grep SECRET_KEY ~/.bashrc &> /dev/null
-if [ $? -ne 0 ]; then
-    SECRET_KEY=`cat /dev/urandom | tr -dc A-Za-z0-9 | head -c 50`
-    echo "SECRET_KEY=$SECRET_KEY" >> ~/.bashrc
-    echo $SECRET_KEY
-else
-    echo_warning SECRET_KEY已存在：${SECRET_KEY}
-fi
-grep BOOTSTRAP_TOKEN ~/.bashrc &> /dev/null
-if [ $? -ne 0 ]; then
-    BOOTSTRAP_TOKEN=`cat /dev/urandom | tr -dc A-Za-z0-9 | head -c 16`
-    echo "BOOTSTRAP_TOKEN=$BOOTSTRAP_TOKEN" >> ~/.bashrc
-    echo $BOOTSTRAP_TOKEN
-else
-    echo_warning BOOTSTRAP_TOKEN已存在：${BOOTSTRAP_TOKEN}
-fi
 
-# docker启动命令
-echo_info 通过docker启动容器，容器名为jms_all
-    #    -v ${jumpserver_data_dir}:/opt/jumpserver/data \
-    #    -v ${jumpserver_mysql_dir}:/var/lib/mysql \
-docker run --name jms_all -d \
-       -p ${web_port}:80 \
-       -p ${ssh_port}:2222 \
-       -e SECRET_KEY=$SECRET_KEY \
-       -e BOOTSTRAP_TOKEN=$BOOTSTRAP_TOKEN \
-       --restart=always \
-       jumpserver/jms_all
+# 首先判断当前目录是否有压缩包：
+#   I. 如果有压缩包，那么就在当前目录解压；
+#   II.如果没有压缩包，那么就检查有没有 ${src_dir} 表示的目录;
+#       1) 如果有目录，那么检查有没有压缩包
+#           ① 有压缩包就解压
+#           ② 没有压缩包则下载压缩包
+#       2) 如果没有,那么就创建这个目录，然后 cd 到目录中，然后下载压缩包，然
+#       后解压
+# 解压的步骤都在后面，故此处只做下载
 
-if [ $? -ne 0 ];then
-    exit 1
-fi
+# 语法： download_tar_gz 保存的目录 下载链接
+# 使用示例： download_tar_gz /data/openssh-update https://mirrors.cloud.tencent.com/openssl/source/openssl-1.1.1h.tar.gz
+function download_tar_gz(){
+    download_file_name=$(echo $2 |  awk -F"/" '{print $NF}')
+    back_dir=$(pwd)
+    file_in_the_dir=''  # 这个目录是后面编译目录的父目录
 
-echo_info jumpserver已启动成功，以下是相关信息：
-echo -e "\033[37m                  端口：${web_port}\033[0m"
-# echo -e "\033[37m                  存放key和token的文件：~/.bashrc ，不要修改文件中的 SECRET_KEY 和 BOOTSTRAP_TOKEN ，这样重新运行该脚本，新生成的jumpserver容器就可以获取之前的配置\033[0m"
-# echo -e "\033[37m                  jumpserver数据目录：$(dirname ${jumpserver_data_dir})/ ，不要删除该目录，这样重新运行该脚本，新生成的jumpserver容器就可以获取之前的配置和数据\033[0m"
+    ls $download_file_name &> /dev/null
+    if [ $? -ne 0 ];then
+        # 进入此处表示脚本所在目录没有压缩包
+        ls -d $1 &> /dev/null
+        if [ $? -ne 0 ];then
+            # 进入此处表示没有${src_dir}目录
+            mkdir -p $1 && cd $1
+            echo_info 下载 $download_file_name 至 $(pwd)/
+            # 检测是否有wget工具
+            if [ ! -f /usr/bin/wget ];then
+                echo_info 安装wget工具
+                yum install -y wget
+            fi
+            wget $2
+            file_in_the_dir=$(pwd)
+            # 返回脚本所在目录，这样这个函数才可以多次使用
+            cd ${back_dir}
+        else
+            # 进入此处表示有${src_dir}目录
+            cd $1
+            ls $download_file_name &> /dev/null
+            if [ $? -ne 0 ];then
+            # 进入此处表示${src_dir}目录内没有压缩包
+                echo_info 下载 $download_file_name 至 $(pwd)/
+                # 检测是否有wget工具
+                if [ ! -f /usr/bin/wget ];then
+                    echo_info 安装wget工具
+                    yum install -y wget
+                fi
+                wget $2
+                file_in_the_dir=$(pwd)
+                cd ${back_dir}
+            else
+                # 进入此处，表示${src_dir}目录内有压缩包
+                echo_info 发现压缩包$(pwd)/$download_file_name
+                file_in_the_dir=$(pwd)
+                cd ${back_dir}
+            fi
+        fi
+    else
+        # 进入此处表示脚本所在目录有压缩包
+        echo_info 发现压缩包$(pwd)/$download_file_name
+        file_in_the_dir=$(pwd)
+    fi
+}
+
+function pre_check() {
+    if [ -d ${JUMP_HOME} ];then
+        echo_error 检测到${JUMP_HOME}目录，请确认是否重复安装
+        exit 1
+    fi
+
+    if [ -f ${SRC_DIR}/master.zip ];then
+        echo_error 检测到${SRC_DIR}/master.zip文件，请确认是否重复安装
+        exit 2
+    fi
+
+    unzip -h &> /dev/null
+    if [ $? -ne 0 ];then
+        echo_info 安装unzip
+        yum install -y unzip
+        if [ $? -ne 0 ];then
+            echo_error 安装unzip失败，退出
+            exit 3
+        fi
+    fi
+}
+
+function get_machine_ip() {
+    ip a | grep -E "bond" &> /dev/null
+    if [ $? -eq 0 ];then
+        echo_warning 检测到绑定网卡（bond），请手动输入使用的 ip ：
+        input_machine_ip_fun
+    elif [ $(ip a | grep -E "inet.*e(ns|np|th).*[[:digit:]]+.*" | awk '{print $2}' | cut -d / -f 1 | wc -l) -gt 1 ];then
+        echo_warning 检测到多个 ip，请手动输入使用的 ip ：
+        input_machine_ip_fun
+    else
+        machine_ip=$(ip a | grep -E "inet.*e(ns|np|th).*[[:digit:]]+.*" | awk '{print $2}' | cut -d / -f 1)
+    fi
+}
+
+function download_and_startup_jumpserver() {
+    download_tar_gz ${SRC_DIR} https://github.com/jumpserver/Dockerfile/archive/refs/heads/master.zip
+    cd ${SRC_DIR}
+    echo_info 解压master.zip
+    unzip master.zip
+
+    mv ${JUMP_GIT_DIR}-master ${MY_DIR}/${JUMP_HOME}
+    rm -f master.zip
+    cd ${MY_DIR}/${JUMP_HOME}
+    cp config_example.conf .env
+
+    echo_info 调整jumpserver配置
+    sed -i "s#80:80#${JUMP_PORT}:80#g" docker-compose.yml
+    sed -i "s#- mysql-data:/var/lib/mysql#- ./data/mysql-data:/var/lib/mysql#g" docker-compose.yml
+    sed -i "s#- redis-data:/var/lib/redis/#- ./data/redis-data:/var/lib/redis/#g" docker-compose.yml
+    sed -i "s#- core-data:/opt/jumpserver/data#- ./data/core-data:/opt/jumpserver/data#g" docker-compose.yml
+    sed -i "s#- koko-data:/opt/koko/data#- ./data/koko-data:/opt/koko/data#g" docker-compose.yml
+    sed -i "s#- guacamole-data:/config/guacamole/data#- ./data/guacamole-data:/config/guacamole/data#g" docker-compose.yml
+
+    sed -i "/^volumes:$/d" docker-compose.yml
+    sed -i "/^  mysql-data:$/d" docker-compose.yml
+    sed -i "/^  redis-data:$/d" docker-compose.yml
+    sed -i "/^  core-data:$/d" docker-compose.yml
+    sed -i "/^  koko-data:$/d" docker-compose.yml
+    sed -i "/^  guacamole-data:$/d" docker-compose.yml
+
+    echo_info 如有更多修改，请编辑 ${JUMP_HOME}/.env 文件
+
+    echo_info 启动jumpserver
+    docker-compose up -d
+    if [ $? -ne 0 ];then
+        echo_error 哦豁，启动报错了，请检查docker-compose.yml文件
+        exit 4
+    fi
+    get_machine_ip
+    echo_info jumpserver已部署成功，访问地址：http://${machine_ip}:${JUMP_PORT}
+    echo -e "\033[37m                  默认账号：admin\033[0m"
+    echo -e "\033[37m                  默认密码：admin\033[0m"
+}
+
+pre_check
+download_and_startup_jumpserver
