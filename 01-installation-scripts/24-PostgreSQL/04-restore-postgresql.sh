393887da770ecbfb657017b04ea91365e834ade2
unknown <946649765@qq.com>
2021-07-16 18:11:20 +0800

    恢复pg后屏蔽掉从库，以免恢复后的pg不能写

--- 01-installation-scripts/24-PostgreSQL/04-restore-postgresql.sh
+++ 01-installation-scripts/24-PostgreSQL/04-restore-postgresql.sh
@@ -77,6 +77,9 @@ function restore_pg() {
     chown -R postgres:postgres ${PG_HOME}/data
     chmod 700 ${PG_HOME}/data
     get_restore_time_range
+    # 如果有从库配置，注释掉从库，否则主库不能写
+    sed -i "s/^synchronous_standby_names/# &/g"
+    
     echo_info 启动postgresql
     systemctl start ${SYSTEM_UNIT_FILE}
 }
