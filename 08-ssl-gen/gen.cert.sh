4ade8ba99a1cf85d44c0912b3767abe8856a437f
unknown <946649765@qq.com>
2021-08-02 14:36:43 +0800

    新增subjectAltName参数的值为本机ip

--- 08-ssl-gen/gen.cert.sh
+++ 08-ssl-gen/gen.cert.sh
@@ -33,6 +33,33 @@ TIME=`date +%Y%m%d-%H%M`
 DIR="${BASE_DIR}/${TIME}"
 mkdir -p ${DIR}
 
+# 替换ca.cnf中subjectAltName的值为本机ip
+#-------------------------------------------------
+function input_machine_ip_fun() {
+    read input_machine_ip
+    machine_ip=${input_machine_ip}
+    if [[ ! $machine_ip =~ ^([0,1]?[0-9]{1,2}|2([0-4][0-9]|5[0-5]))(\.([0,1]?[0-9]{1,2}|2([0-4][0-9]|5[0-5]))){3} ]];then
+        echo_error 错误的ip格式，退出
+        exit 7
+    fi
+}
+function get_machine_ip() {
+    ip a | grep -E "bond" &> /dev/null
+    if [ $? -eq 0 ];then
+        echo_warning 检测到绑定网卡（bond），请手动输入使用的 ip ：
+        input_machine_ip_fun
+    elif [ $(ip a | grep -E "inet.*e(ns|np|th).*[[:digit:]]+.*" | awk '{print $2}' | cut -d / -f 1 | wc -l) -gt 1 ];then
+        echo_warning 检测到多个 ip，请手动输入使用的 ip ：
+        input_machine_ip_fun
+    else
+        machine_ip=$(ip a | grep -E "inet.*e(ns|np|th).*[[:digit:]]+.*" | awk '{print $2}' | cut -d / -f 1)
+    fi
+}
+
+get_machine_ip
+sed -i "s/subjectAltName = IP:.*/subjectAltName = IP:${machine_ip}/g" ca.cnf
+#-------------------------------------------------
+
 # Create CSR
 openssl req -new -out "${DIR}/$1.csr.pem" \
     -key out/cert.key.pem \
