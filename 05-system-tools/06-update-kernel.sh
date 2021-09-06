b8c7f1942db9d1bc356c528213818bef7e0dd07c
zhegeshijiehuiyouai <946649765@qq.com>
2021-09-06 14:32:49 +0800

    添加rpm包是否存在的检测

--- 05-system-tools/06-update-kernel.sh
+++ 05-system-tools/06-update-kernel.sh
@@ -1,8 +1,85 @@
 #!/bin/bash
 
 KERNEL_VERSION="5.4.141-1"
+DOWNLOAD_DIR=00src00
 
-wget https://elrepo.org/linux/kernel/el7/x86_64/RPMS/kernel-lt-${KERNEL_VERSION}.el7.elrepo.x86_64.rpm
+
+# 带格式的echo函数
+function echo_info() {
+    echo -e "[\033[36m$(date +%T)\033[0m] [\033[32mINFO\033[0m] \033[37m$@\033[0m"
+}
+function echo_warning() {
+    echo -e "[\033[36m$(date +%T)\033[0m] [\033[1;33mWARNING\033[0m] \033[1;37m$@\033[0m"
+}
+function echo_error() {
+    echo -e "[\033[36m$(date +%T)\033[0m] [\033[41mERROR\033[0m] \033[1;31m$@\033[0m"
+}
+
+# 首先判断当前目录是否有压缩包：
+#   I. 如果有压缩包，那么就在当前目录解压；
+#   II.如果没有压缩包，那么就检查有没有 ${src_dir} 表示的目录;
+#       1) 如果有目录，那么检查有没有压缩包
+#           ① 有压缩包就解压
+#           ② 没有压缩包则下载压缩包
+#       2) 如果没有,那么就创建这个目录，然后 cd 到目录中，然后下载压缩包，然
+#       后解压
+# 解压的步骤都在后面，故此处只做下载
+
+# 语法： download_tar_gz 保存的目录 下载链接
+# 使用示例： download_tar_gz /data/openssh-update https://mirrors.cloud.tencent.com/openssl/source/openssl-1.1.1h.tar.gz
+function download_tar_gz(){
+    download_file_name=$(echo $2 |  awk -F"/" '{print $NF}')
+    back_dir=$(pwd)
+    file_in_the_dir=''  # 这个目录是后面编译目录的父目录
+
+    ls $download_file_name &> /dev/null
+    if [ $? -ne 0 ];then
+        # 进入此处表示脚本所在目录没有压缩包
+        ls -d $1 &> /dev/null
+        if [ $? -ne 0 ];then
+            # 进入此处表示没有${src_dir}目录
+            mkdir -p $1 && cd $1
+            echo_info 下载 $download_file_name 至 $(pwd)/
+            # 检测是否有wget工具
+            if [ ! -f /usr/bin/wget ];then
+                echo_info 安装wget工具
+                yum install -y wget
+            fi
+            wget $2
+            file_in_the_dir=$(pwd)
+            # 返回脚本所在目录，这样这个函数才可以多次使用
+            cd ${back_dir}
+        else
+            # 进入此处表示有${src_dir}目录
+            cd $1
+            ls $download_file_name &> /dev/null
+            if [ $? -ne 0 ];then
+            # 进入此处表示${src_dir}目录内没有压缩包
+                echo_info 下载 $download_file_name 至 $(pwd)/
+                # 检测是否有wget工具
+                if [ ! -f /usr/bin/wget ];then
+                    echo_info 安装wget工具
+                    yum install -y wget
+                fi
+                wget $2
+                file_in_the_dir=$(pwd)
+                cd ${back_dir}
+            else
+                # 进入此处，表示${src_dir}目录内有压缩包
+                echo_info 发现压缩包$(pwd)/$download_file_name
+                file_in_the_dir=$(pwd)
+                cd ${back_dir}
+            fi
+        fi
+    else
+        # 进入此处表示脚本所在目录有压缩包
+        echo_info 发现压缩包$(pwd)/$download_file_name
+        file_in_the_dir=$(pwd)
+    fi
+}
+
+download_tar_gz ${DOWNLOAD_DIR} https://elrepo.org/linux/kernel/el7/x86_64/RPMS/kernel-lt-${KERNEL_VERSION}.el7.elrepo.x86_64.rpm
+
 rpm -ivh kernel-lt-${KERNEL_VERSION}.el7.elrepo.x86_64.rpm
 cat /boot/grub2/grub.cfg | grep menuentry
 grub2-set-default "CentOS Linux (${KERNEL_VERSION}.el7.elrepo.x86_64) 7 (Core)"
