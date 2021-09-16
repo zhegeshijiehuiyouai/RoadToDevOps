d99d8558eb027863cdc97b9541f20f38bd936206
zhegeshijiehuiyouai <946649765@qq.com>
2021-09-16 21:48:38 +0800

    指定docker-compose版本为v1最后一个版本

--- 01-installation-scripts/04-Docker/01-install-docker.sh
+++ 01-installation-scripts/04-Docker/01-install-docker.sh
@@ -59,20 +59,20 @@ docker -v
 ######## 部署docker-compose
 echo_info 部署docker-compose中，请耐心等候
 
-curl_timeout=5
-# 设置dns超时时间，避免没网情况下等很久
-echo "options timeout:${curl_timeout} attempts:1 rotate" >> /etc/resolv.conf
-docker_compose_version=$(curl -s -H "User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36" --connect-timeout ${curl_timeout} https://github.com/docker/compose/tags | grep "/docker/compose/releases/tag/" | head -1 | awk -F'"' '{print $2}' | xargs basename)
-# 接口正常，[ ! ${docker_compose_version} ]为1；接口失败，[ ! ${docker_compose_version} ]为0
-if [ ! ${docker_compose_version} ];then
-    echo_error docker-compose github官网[ https://github.com/docker/compose/tags ]访问超时，请检查网络！
-    sed -i '$d' /etc/resolv.conf
-    exit 10
-fi
-sed -i '$d' /etc/resolv.conf
-# 2020.12.13测试发现，当前最新版本部署有bug，故手动指定docker-compose版本
-# 2021.03.08测试已ok，并新增请求头
-# docker_compose_version=1.28.5
+# curl_timeout=4
+# # 设置dns超时时间，避免没网情况下等很久
+# echo "options timeout:${curl_timeout} attempts:1 rotate" >> /etc/resolv.conf
+# docker_compose_version=$(curl -s -H "User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36" --connect-timeout ${curl_timeout} https://github.com/docker/compose/tags | grep "/docker/compose/releases/tag/" | head -1 | awk -F'"' '{print $2}' | xargs basename)
+# # 接口正常，[ ! ${docker_compose_version} ]为1；接口失败，[ ! ${docker_compose_version} ]为0
+# if [ ! ${docker_compose_version} ];then
+#     echo_error docker-compose github官网[ https://github.com/docker/compose/tags ]访问超时，请检查网络！
+#     sed -i '$d' /etc/resolv.conf
+#     exit 10
+# fi
+# sed -i '$d' /etc/resolv.conf
+
+# docker-compose v2命令和v1不一样了，故使用老版本
+docker_compose_version=1.29.2
 
 back_task=/tmp/.display_dot_to_show_aliviness
 # 显示变化小点，表示没有卡死
@@ -102,7 +102,7 @@ EOF
 /bin/bash $back_task &
 
 # 使用国内源加速下载
-curl -sL --connect-timeout 5 "https://get.daocloud.io/docker/compose/releases/download/${docker_compose_version}/docker-compose-linux-amd64" -o /usr/local/bin/docker-compose
+curl -sL --connect-timeout 5 "https://get.daocloud.io/docker/compose/releases/download/${docker_compose_version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 echo  # 换行，与小点隔开
 echo_info docker-compose已部署成功，版本信息如下：
