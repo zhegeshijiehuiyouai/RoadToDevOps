482203d8eda7c32963750e07d6d60affd8284131
zhegeshijiehuiyouai <946649765@qq.com>
2021-09-27 21:12:12 +0800

    船新版本的kernel升级脚本

--- 05-system-tools/06-update-kernel.sh
+++ 05-system-tools/06-update-kernel.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-KERNEL_VERSION="$(curl -s --connect-timeout 5 https://elrepo.org/linux/kernel/el7/x86_64/RPMS/ | grep "kernel-lt-[0-9].*" | awk -F 'href="' '{print $2}' | awk -F '"' '{print $1}' | tail -1 |awk -F "-" '{print $3}')-1"
-DOWNLOAD_DIR=00src00
-
-
 # 带格式的echo函数
 function echo_info() {
     echo -e "[\033[36m$(date +%T)\033[0m] [\033[32mINFO\033[0m] \033[37m$@\033[0m"
@@ -15,83 +11,47 @@ function echo_error() {
     echo -e "[\033[36m$(date +%T)\033[0m] [\033[41mERROR\033[0m] \033[1;31m$@\033[0m"
 }
 
-# 首先判断当前目录是否有压缩包：
-#   I. 如果有压缩包，那么就在当前目录解压；
-#   II.如果没有压缩包，那么就检查有没有 ${src_dir} 表示的目录;
-#       1) 如果有目录，那么检查有没有压缩包
-#           ① 有压缩包就解压
-#           ② 没有压缩包则下载压缩包
-#       2) 如果没有,那么就创建这个目录，然后 cd 到目录中，然后下载压缩包，然
-#       后解压
-# 解压的步骤都在后面，故此处只做下载
+echo_info 添加镜像源
+rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
 
-# 语法： download_tar_gz 保存的目录 下载链接
-# 使用示例： download_tar_gz /data/openssh-update https://mirrors.cloud.tencent.com/openssl/source/openssl-1.1.1h.tar.gz
-function download_tar_gz(){
-    download_file_name=$(echo $2 |  awk -F"/" '{print $NF}')
-    back_dir=$(pwd)
-    file_in_the_dir=''  # 这个目录是后面编译目录的父目录
+echo_info 安装最新内核RPM包
+yum --enablerepo=elrepo-kernel install kernel-ml -y
 
-    ls $download_file_name &> /dev/null
-    if [ $? -ne 0 ];then
-        # 进入此处表示脚本所在目录没有压缩包
-        ls -d $1 &> /dev/null
-        if [ $? -ne 0 ];then
-            # 进入此处表示没有${src_dir}目录
-            mkdir -p $1 && cd $1
-            echo_info 下载 $download_file_name 至 $(pwd)/
-            # 检测是否有wget工具
-            if [ ! -f /usr/bin/wget ];then
-                echo_info 安装wget工具
-                yum install -y wget
-            fi
-            wget $2
-            if [ $? -ne 0 ];then
-                echo_error 下载 $2 失败！
-                exit 1
-            fi
-            file_in_the_dir=$(pwd)
-            # 返回脚本所在目录，这样这个函数才可以多次使用
-            cd ${back_dir}
-        else
-            # 进入此处表示有${src_dir}目录
-            cd $1
-            ls $download_file_name &> /dev/null
-            if [ $? -ne 0 ];then
-            # 进入此处表示${src_dir}目录内没有压缩包
-                echo_info 下载 $download_file_name 至 $(pwd)/
-                # 检测是否有wget工具
-                if [ ! -f /usr/bin/wget ];then
-                    echo_info 安装wget工具
-                    yum install -y wget
-                fi
-                wget $2
-                if [ $? -ne 0 ];then
-                    echo_error 下载 $2 失败！
-                    exit 1
-                fi
-                file_in_the_dir=$(pwd)
-                cd ${back_dir}
-            else
-                # 进入此处，表示${src_dir}目录内有压缩包
-                echo_info 发现压缩包$(pwd)/$download_file_name
-                file_in_the_dir=$(pwd)
-                cd ${back_dir}
-            fi
-        fi
-    else
-        # 进入此处表示脚本所在目录有压缩包
-        echo_info 发现压缩包$(pwd)/$download_file_name
-        file_in_the_dir=$(pwd)
-    fi
-}
 
-download_tar_gz ${DOWNLOAD_DIR} https://elrepo.org/linux/kernel/el7/x86_64/RPMS/kernel-lt-${KERNEL_VERSION}.el7.elrepo.x86_64.rpm
+echo_info 设置内核
+# 按照版本号对kernel进行排序
+newest_kernel=$(egrep ^menuentry /etc/grub2.cfg | cut -f 2 -d \' | awk -F "(" '{print $2}' | awk -F ")" '{print $1}' | sort -V | tail -1)
+newest_kernel_id=$(egrep ^menuentry /etc/grub2.cfg | cut -f 2 -d \' | cat -n | grep ${newest_kernel} | awk '{print $1}' | head -1)
+newest_kernel_id=$((newest_kernel_id - 1))
+grub2-set-default ${newest_kernel_id}
 
-cd ${file_in_the_dir}
-rpm -ivh kernel-lt-${KERNEL_VERSION}.el7.elrepo.x86_64.rpm
-cat /boot/grub2/grub.cfg | grep menuentry
-grub2-set-default "CentOS Linux (${KERNEL_VERSION}.el7.elrepo.x86_64) 7 (Core)"
-grub2-editenv list
-grub2-mkconfig -o /boot/grub2/grub.cfg
-reboot
+
+echo_info 开启BBR算法
+echo 'net.core.default_qdisc=fq' > /etc/sysctl.d/bbr.conf
+echo 'net.ipv4.tcp_congestion_control=bbr' >> /etc/sysctl.d/bbr.conf
+
+echo_info 内核参数优化
+cat > /etc/sysctl.d/bbr.conf <<EOF
+net.ipv4.conf.default.rp_filter=1
+net.ipv4.conf.all.rp_filter=1
+net.ipv4.ip_forward=1
+net.ipv4.tcp_syncookies=1
+
+net.ipv6.conf.all.disable_ipv6=1
+net.ipv6.conf.default.disable_ipv6=1
+net.ipv6.conf.lo.disable_ipv6=1
+EOF
+
+sysctl -p
+
+echo_info "是否重启服务器 [Y/n]"
+read is_reboot
+case ${is_reboot} in
+    y|Y)
+        reboot
+        ;;
+    *)
+        exit 0
+        ;;
+esac
