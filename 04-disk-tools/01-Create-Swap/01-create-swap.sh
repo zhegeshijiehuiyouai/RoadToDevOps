736603d886c9f6fcde97d64cf359aacaed3a3bea
zhegeshijiehuiyouai <946649765@qq.com>
2021-11-03 13:30:53 +0800

    添加创建swap交互信息，优化/etc/fstab显示

--- 04-disk-tools/01-Create-Swap/01-create-swap.sh
+++ 04-disk-tools/01-Create-Swap/01-create-swap.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# 单位只能为G
+SWAP_SIZE=2G
+SWAP_DIR=$(pwd)/swap
 
 # 带格式的echo函数
 function echo_info() {
@@ -12,20 +15,41 @@ function echo_error() {
     echo -e "[\033[36m$(date +%T)\033[0m] [\033[41mERROR\033[0m] \033[1;31m$@\033[0m"
 }
 
-SWAPDIR=`pwd`
+if [[ ! ${SWAP_SIZE} =~ G$ ]];then
+    echo_error swap单位设置错误
+    exit 1
+fi
+
+echo_info 请确认信息，如需调整请修改脚本
+echo "swap大小：${SWAP_SIZE}"
+echo "swap文件路径：${SWAP_DIR}"
+echo_info "确认(y|N)："
+read USER_INPUT
+case ${USER_INPUT} in
+    y|Y|yes)
+        echo
+        ;;
+    *)
+        exit 2
+        ;;
+esac
 
 echo_info 设置swap亲和度为30
 sysctl -w vm.swappiness=30
 sed -i 's/vm.swappiness = 0/vm.swappiness = 30/g' /etc/sysctl.conf
 
-echo_info 创建一个2G大小的swap文件：${SWAPDIR}/swap
-dd if=/dev/zero of=${SWAPDIR}/swap bs=512 count=4194304
-mkswap ${SWAPDIR}/swap
-chmod 0600 ${SWAPDIR}/swap
+echo_info "swap文件${SWAP_DIR}(${SWAP_SIZE})创建中，请耐心等待..."
+
+SWAP_SIZE=$(echo ${SWAP_SIZE} | cut -d"G" -f 1)
+DD_BS=$((512*${SWAP_SIZE}))
+dd if=/dev/zero of=${SWAP_DIR} bs=${DD_BS} count=2097152
+
+mkswap ${SWAP_DIR}
+chmod 0600 ${SWAP_DIR}
 
 echo_info 挂载swap
-swapon ${SWAPDIR}/swap
-echo "${SWAPDIR}/swap swap swap defaults    0  0" >> /etc/fstab
+swapon ${SWAP_DIR}
+echo -e "${SWAP_DIR}\t\tswap\t\t\tswap\tdefaults\t0 0" >> /etc/fstab
 
 echo_info swap已创建成功
 free -h
