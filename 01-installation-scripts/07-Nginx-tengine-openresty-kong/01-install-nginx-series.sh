346bc4b72763be6379f7a89d5807e5fc60990cde
zhegeshijiehuiyouai <946649765@qq.com>
2022-02-28 15:11:25 +0800

    更新nginx默认版本

--- 01-installation-scripts/07-Nginx-tengine-openresty-kong/01-install-nginx-series.sh
+++ 01-installation-scripts/07-Nginx-tengine-openresty-kong/01-install-nginx-series.sh
@@ -23,7 +23,7 @@ echo_info 从官网获取最新版本中
 version_nginx_hint="（官网最新版）"
 version_tengine_hint="（官网最新版）"
 
-nginx_default_version=1.20.1
+nginx_default_version=1.20.2
 # nginx的版本(从官网获取最新版)
 curl_timeout=2
 # 设置dns超时时间，避免没网情况下等很久
