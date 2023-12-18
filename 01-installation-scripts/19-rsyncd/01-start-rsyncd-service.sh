4d21bf3547f935de1b873afd6432e151bbbe5525
zhegeshijiehuiyouai <946649765@qq.com>
2023-12-18 15:14:44 +0800

    完善客户端命令

--- 01-installation-scripts/19-rsyncd/01-start-rsyncd-service.sh
+++ 01-installation-scripts/19-rsyncd/01-start-rsyncd-service.sh
@@ -189,6 +189,7 @@ function echo_summary() {
     echo_info 生产环境客户端同步命令：
     echo -e "\033[45m[ -d /etc/rsync.d ] || mkdir -p /etc/rsync.d\033[0m"
     echo -e "\033[45mecho \""${rsyncd_password}"\" > /etc/rsync.d/rsync.password\033[0m"
+    echo -e "\033[45mchmod 600 /etc/rsync.d/rsync.password\033[0m"
     echo -e "\033[45mrsync -avz --delete --password-file=/etc/rsync.d/rsync.password 客户端文件 ${rsyncd_user}@${machine_ip}::${formated_share_dir[0]}\033[0m"
 }
 
