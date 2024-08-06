fdf1f37b1b696880b36b122af60f683a0447c30d
zhegeshijiehuiyouai <946649765@qq.com>
2024-08-06 13:58:42 +0800

    更新docker镜像仓库地址

--- 01-installation-scripts/04-Docker/01-install-docker.sh
+++ 01-installation-scripts/04-Docker/01-install-docker.sh
@@ -46,7 +46,7 @@ function adjust_docker_configuration() {
     cd /etc/docker
     cat > daemon.json << EOF
 {
-    "registry-mirrors": ["https://docker.1panel.live/"],
+    "registry-mirrors": ["https://dockerpull.com/"],
     "insecure-registries":["172.21.100.16:9998"],
     "data-root": "/data/docker",
     "log-opts": {"max-size":"10m", "max-file":"1"}
