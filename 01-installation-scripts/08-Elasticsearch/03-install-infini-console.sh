94122bb74919498af0b6c9eb2461ccf12df7d001
zhegeshijiehuiyouai <946649765@qq.com>
2025-06-22 16:18:40 +0800

    添加infini console安装脚本

---
+++ 01-installation-scripts/08-Elasticsearch/03-install-infini-console.sh
@@ -0,0 +1,186 @@
+#!/bin/bash
+# 该脚本用于安装 Infinilabs Easysearch 控制台
+
+
+CONSOLE_VERSION=1.29.6-2148
+CONSOLE_HOME=/data/console
+CONSOLE_PORT=9000
+# 包下载目录
+src_dir=$(pwd)/00src00
+
+download_url=https://release.infinilabs.com/console/stable/console-${CONSOLE_VERSION}-linux-amd64.tar.gz
+
+# 带格式的echo函数
+function echo_info() {
+    echo -e "[\033[36m$(date +%T)\033[0m] [\033[32mINFO\033[0m] \033[37m$@\033[0m"
+}
+function echo_warning() {
+    echo -e "[\033[36m$(date +%T)\033[0m] [\033[1;33mWARNING\033[0m] \033[1;37m$@\033[0m"
+}
+function echo_error() {
+    echo -e "[\033[36m$(date +%T)\033[0m] [\033[41mERROR\033[0m] \033[1;31m$@\033[0m"
+}
+
+# 脚本执行用户检测
+if [[ $(whoami) != 'root' ]];then
+    echo_error 请使用root用户执行
+    exit 99
+fi
+
+# 检测操作系统
+if grep -qs "ubuntu" /etc/os-release; then
+	os="ubuntu"
+    os_version=$(grep 'VERSION_ID' /etc/os-release | cut -d '"' -f 2)
+    # 阻止配置更新弹窗
+    export UCF_FORCE_CONFFOLD=1
+    # 阻止应用重启弹窗
+    export NEEDRESTART_SUSPEND=1
+elif [[ -e /etc/centos-release ]]; then
+    os="centos"
+    os_version=$(grep -oE '([0-9]+\.[0-9]+(\.[0-9]+)?)' /etc/centos-release)
+elif [[ -e /etc/rocky-release ]]; then
+    os="rocky"
+    os_version=$(grep -oE '([0-9]+\.[0-9]+(\.[0-9]+)?)' /etc/rocky-release)
+elif [[ -e /etc/almalinux-release ]]; then
+    os="alma"
+    os_version=$(grep -oE '([0-9]+\.[0-9]+(\.[0-9]+)?)' /etc/almalinux-release)
+else
+	true
+fi
+
+function download_tar_gz(){
+    download_file_name=$(echo $2 |  awk -F"/" '{print $NF}')
+    back_dir=$(pwd)
+    file_in_the_dir=''  # 这个目录是后面编译目录的父目录
+
+    ls $download_file_name &> /dev/null
+    if [ $? -ne 0 ];then
+        # 进入此处表示脚本所在目录没有压缩包
+        ls -d $1 &> /dev/null
+        if [ $? -ne 0 ];then
+            # 进入此处表示没有${src_dir}目录
+            mkdir -p $1 && cd $1
+            echo_info 下载 $download_file_name 至 $(pwd)/
+            # 检测是否有wget工具
+            if [ ! -f /usr/bin/wget ];then
+                echo_info 安装wget工具
+                if [[ $os == "centos" ]];then
+                    yum install -y wget
+                elif [[ $os == "ubuntu" ]];then
+                    apt install -y wget
+                elif [[ $os == 'rocky' || $os == 'alma' ]];then
+                    dnf install -y wget
+                fi
+            fi
+            check_downloadfile $2
+            wget --no-check-certificate $2
+            if [ $? -ne 0 ];then
+                echo_error 下载 $2 失败！
+                exit 1
+            fi
+            file_in_the_dir=$(pwd)
+            # 返回脚本所在目录，这样这个函数才可以多次使用
+            cd ${back_dir}
+        else
+            # 进入此处表示有${src_dir}目录
+            cd $1
+            ls $download_file_name &> /dev/null
+            if [ $? -ne 0 ];then
+            # 进入此处表示${src_dir}目录内没有压缩包
+                echo_info 下载 $download_file_name 至 $(pwd)/
+                # 检测是否有wget工具
+                if [ ! -f /usr/bin/wget ];then
+                    echo_info 安装wget工具
+                    if [[ $os == "centos" ]];then
+                        yum install -y wget
+                    elif [[ $os == "ubuntu" ]];then
+                        apt install -y wget
+                    elif [[ $os == 'rocky' || $os == 'alma' ]];then
+                        dnf install -y wget
+                    fi
+                fi
+                check_downloadfile $2
+                wget --no-check-certificate $2
+                if [ $? -ne 0 ];then
+                    echo_error 下载 $2 失败！
+                    exit 1
+                fi
+                file_in_the_dir=$(pwd)
+                cd ${back_dir}
+            else
+                # 进入此处，表示${src_dir}目录内有压缩包
+                echo_info 发现压缩包$(pwd)/$download_file_name
+                file_in_the_dir=$(pwd)
+                cd ${back_dir}
+            fi
+        fi
+    else
+        # 进入此处表示脚本所在目录有压缩包
+        echo_info 发现压缩包$(pwd)/$download_file_name
+        file_in_the_dir=$(pwd)
+    fi
+}
+
+#-------------------------------------------------
+function input_machine_ip_fun() {
+    read input_machine_ip
+    machine_ip=${input_machine_ip}
+    if [[ ! $machine_ip =~ ^([0,1]?[0-9]{1,2}|2([0-4][0-9]|5[0-5]))(\.([0,1]?[0-9]{1,2}|2([0-4][0-9]|5[0-5]))){3} ]];then
+        echo_error 错误的ip格式，退出
+        exit 1
+    fi
+}
+function get_machine_ip() {
+    ip a | grep -E "bond" &> /dev/null
+    if [ $? -eq 0 ];then
+        echo_warning 检测到绑定网卡（bond），请手动输入使用的 ip ：
+        input_machine_ip_fun
+    elif [ $(ip a | grep -E "inet.*e(ns|np|th).*[[:digit:]]+.*" | awk '{print $2}' | cut -d / -f 1 | wc -l) -gt 1 ];then
+        echo_warning 检测到多个 ip，请手动输入使用的 ip ：
+        input_machine_ip_fun
+    else
+        machine_ip=$(ip a | grep -E "inet.*e(ns|np|th).*[[:digit:]]+.*" | awk '{print $2}' | cut -d / -f 1)
+    fi
+}
+#-------------------------------------------------
+
+function config_console() {
+    echo_info "配置 console"
+    console_yml_file=${CONSOLE_HOME}/console.yml
+    get_machine_ip
+    sed -i 's/^    binding: 0.0.0.0:.*/    binding: 0.0.0.0:'${CONSOLE_PORT}'/g' ${console_yml_file}
+}
+
+
+function echo_summary() {
+    echo_info console 已部署完毕，以下是相关信息：
+    echo -e "\033[37m                  启动命令：systemctl start console\033[0m"
+    echo -e "\033[37m                  console地址：http://${machine_ip}:${CONSOLE_PORT}\033[0m"
+    echo
+}
+
+if [[ -d ${CONSOLE_HOME} ]]; then
+    echo_error "目录 ${CONSOLE_HOME} 已存在，可能之前安装过 console 服务，请检查"
+    exit 2
+else
+    echo_info "创建 console 安装目录"
+    mkdir -p ${CONSOLE_HOME}
+fi
+
+echo_info "开始下载 console ${CONSOLE_VERSION} 包"
+download_tar_gz ${src_dir} ${download_url}
+cd ${src_dir}
+echo_info "解压 console ${CONSOLE_VERSION} 包"
+tar -zxf console-${CONSOLE_VERSION}-linux-amd64.tar.gz -C ${CONSOLE_HOME}
+
+cd ${CONSOLE_HOME}
+echo_info "配置服务后台运行"
+./console-linux-amd64 -service install
+
+config_console
+
+echo_info "启动 console"
+systemctl daemon-reload
+systemctl start console
+
+echo_summary
